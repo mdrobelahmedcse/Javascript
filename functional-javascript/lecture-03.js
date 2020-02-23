d3ef9ad7c052dc91f171259508b2484889d001c3
unknown <you@example.com>
2020-02-23 15:37:19 +0600

    inner function

---
+++ functional-javascript/lecture-03.js
@@ -0,0 +1,48 @@
+/****************************
+ * 
+ * Functional Javascript
+ * inner Function
+ * Lectur - 3
+ * 
+ *****************************/
+
+
+function outer() {
+
+    var a = 10;
+
+    function inner() {
+        a;
+        console.log('I am Inner Function');
+    }
+    inner();
+    console.log('I am Outer function');
+}
+
+
+
+outer();
+
+function add (a, b) {
+
+    function sum(){
+        return a+b;
+    }
+
+    function sub(){
+        return a-b;
+    }
+
+    function times() {
+        return a*b;
+    }
+
+    function div() {
+        return a/b;
+    }
+
+    return sum() + sub() + times() + div();
+}
+
+var result = add(10, 5);
+console.log(result);
