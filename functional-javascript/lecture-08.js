7f0720f435fa819828c82d6f7ff3756d836cd171
unknown <you@example.com>
2020-02-23 15:44:29 +0600

    clouser

---
+++ functional-javascript/lecture-08.js
@@ -0,0 +1,25 @@
+/****************************
+ * 
+ * Functional Javascript
+ * Clouser
+ * Lectur - 8
+ * 
+ *****************************/
+
+
+ var name = 'Twinkle Cats';
+
+function sayName() {
+    console.log('Hello, ' + name);
+}
+// console.dir(sayName);
+sayName();
+
+function greeting(msg) {
+    return function(name) {
+        console.log(msg + ' ' + name);
+    }
+}
+
+var hello = greeting('Hello');
+hello('Twinkle Cats');
